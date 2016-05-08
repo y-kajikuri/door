d3c7bd0ac8585df1613ada755f98aeeb080fb94d
y-Kajikuri <y-kajikuri@unext.jp>
2016-05-08 15:18:32 +0900

    2つのドア状態をレスポンスするように変更

--- src/main/webapp/js/getdootstate.js
+++ src/main/webapp/js/getdootstate.js
@@ -6,8 +6,10 @@ $(document).ready(function(){
 	   url: "http://10.4.129.241:9001/door/select",
 	   success: function(msg){
 	     console.log(msg[0]['doorStatus']);
-	     var state = msg[0]['doorStatus'];
-	     $("#doorStatus").html(state);
+	      var state61 = msg[0]['doorStatus'];
+	      var state62 = msg[1]['doorStatus'];
+	     $("#doorStatus61").html(state61);
+	     $("#doorStatus62").html(state62);
 	   }
 	 });
 });
